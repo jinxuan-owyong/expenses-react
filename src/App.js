239f9e1890e4c591f4ca95ef2f349bf089a13c20
Jin Xuan Ow Yong <jinxuan.owyong@gmail.com>
2022-05-09 03:51:02 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Expenses from './components/Expenses/Expenses';
+
+function App() {
+  return (
+    <div>
+      <h2>Let&apos;s get started!</h2>
+      <Expenses />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import './ExpenseItem.css';
+import Card from '../UI/Card';
+import ExpenseDate from './ExpenseDate';
+
+const ExpenseItem = function (props) {
+  const { date, title, amount } = props;
+
+  return (
+    <Card className="expense-item">
+      <div className="expense-item__description">
+        <ExpenseDate date={date} />
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+      </div>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
