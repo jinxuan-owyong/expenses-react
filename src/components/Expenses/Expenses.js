afcc8805af66fbc255e60d2eed8240426e748ea5
Jin Xuan Ow Yong <jinxuan.owyong@gmail.com>
2022-05-14 16:07:30 +0800

    Add filter items by year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,43 +1,60 @@
-import React from 'react';
-import './Expenses.css';
-import ExpenseItem from './ExpenseItem';
+import React, { useState } from "react";
+import "./Expenses.css";
+import Card from "../UI/Card";
+import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "./ExpensesFilter";
 
 const Expenses = function () {
   const expenses = [
     {
-      id: 'e1',
-      title: 'Toilet Paper',
+      id: "e1",
+      title: "Toilet Paper",
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
     {
-      id: 'e3',
-      title: 'Car Insurance',
+      id: "e3",
+      title: "Car Insurance",
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
+      id: "e4",
+      title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
     },
   ];
 
+  const processExpenses = function (exp) {
+    return exp.map((e) => {
+      return (
+        <ExpenseItem
+          key={e.id}
+          title={e.title}
+          amount={e.amount}
+          date={e.date}
+        />
+      );
+    });
+  };
+
+  const [filteredItems, setFilteredItems] = useState(processExpenses(expenses));
+  const handleYearChange = function (year) {
+    const filteredItems = expenses.filter(
+      (expense) => expense.date.getFullYear() === +year
+    );
+    setFilteredItems(processExpenses(filteredItems));
+  };
+
   return (
-    <div className="expenses">
-      {expenses.map((exp) => {
-        return (
-          <ExpenseItem
-            key={exp.id}
-            title={exp.title}
-            amount={exp.amount}
-            date={exp.date}
-          />
-        );
-      })}
-    </div>
+    <Card className="expenses">
+      <div>
+        <ExpenseFilter onYearChange={handleYearChange} />
+        {filteredItems}
+      </div>
+    </Card>
   );
 };
 
